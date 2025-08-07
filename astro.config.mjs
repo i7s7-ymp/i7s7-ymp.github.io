2faec0974b5d53bfb90c0636e021d83494d00df7
Hiroki Yamashita <ruler0510@gmail.com>
2025-08-07 13:28:30 +0900

    rm hugo and init astro

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://i7s7-ymp.github.io',
+  output: 'static'
+});
