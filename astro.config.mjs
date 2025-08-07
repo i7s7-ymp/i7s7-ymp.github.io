d98269712dc234ab4fa10979149265726777e073
Hiroki Yamashita <ruler0510@gmail.com>
2025-08-07 05:12:28 +0000

    devcontainer.jsonのポートを3000に変更し、astro.config.mjsにサーバーポートを追加; package.hugo.jsonを削除し、package.jsonを整理

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://i7s7-ymp.github.io',
-  output: 'static'
+  output: 'static',
+  server: {
+    port: 3000
+  }
 });
